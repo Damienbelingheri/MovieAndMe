5405538773762f8b76374930e5374cb81f1e0629
Damien Belingheri <belingheridamien@gmail.com>
2021-05-06 02:04:28 +0200

    first commit

---
+++ Components/FilmItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function FilmItem() {
+  return (
+    <View style={styles.main_container}>
+      <Text style={styles.title_text}>Titre du film</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    height: 190,
+  },
+  title_text: {},
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+https://openclassrooms.com/fr/courses/4902061-developpez-une-application-mobile-react-native
